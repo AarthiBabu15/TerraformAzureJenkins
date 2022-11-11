8f7cc525db0b5dc5da6f0bfb93c5176fbcf7311f
AarthiBabu15 <117907429+AarthiBabu15@users.noreply.github.com>
2022-11-11 14:36:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resoure "myfirstresource" "rg-name" {
-    prefix = var.resource_group_name_prefix
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-resource "azure_resource_group" "rg" {
-    name = myfirstresource.rg-name.id
-    location = var.resource_group_location
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
